302fa05ffa9c9df6b2f5189be4911e5fddc90780
ShanchaiZ <shanchai.zahid@gmail.com>
2022-08-16 17:47:23 -0400

    Implementing user input of numbers as a prompt

---
+++ script.js
@@ -0,0 +1,6 @@
+//Accepting User Input of numbers
+//========================================================
+//1. Prompt user to enter a maximum number
+//2. Convert user input into an interger --> Console.log maximum to check function = Works!
+let maximum = parseInt(prompt("Enter the maximum number!"));
+console.log(maximum);
