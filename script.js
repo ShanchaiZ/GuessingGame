d3726e08d64c3ddf215295dfbab8f20ff875c65a
ShanchaiZ <shanchai.zahid@gmail.com>
2022-08-16 19:01:39 -0400

    implemented number of attempts

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-//Accepting User Input of numbers
-//========================================================
-//1. Prompt user to enter a maximum number
-//2. Convert user input into an interger --> Console.log maximum to check function = Works!
+//User Input of numbers
+//===============================================================================================
+//1. Prompt the user to enter a maximum number because this will create the range of numbers for the users to guess from.
+//2. Convert user input into an interger using ParseInt and Prompt --> Console.log maximum to check function = Works!
 let maximum = parseInt(prompt("Please Enter the maximum number!"));
 
-//Verify if userInput number is a valid number using a WHILE loop. IF LOOP only checks validity ONCE. WHILE loop will check the validity of number continously.
+//Verify if user Input number is a valid number using a WHILE loop. IF LOOPs only checks validity ONCE, but WHILE loop will check the validity of number continously.
 while (!maximum) {
     maximum = parseInt(prompt("Enter a VALID number please!"));
 };
 
-//3. Generate Random Number using the prompted number:
+//Generate Random Number using the prompted maximum input:
+//===============================================================================================
 let targetNum = Math.floor((Math.random() * maximum) + 1);
-console.log(targetNum);
+console.log(targetNum); //used to test if game works
+let attempts = 1;
 
 
-//Create Game Logic:
+//4. Create Game Logic:
 let guess = parseInt(prompt("Enter your first guess!"));
 
 while (guess !== targetNum) {
-    if (guess > targetNum){
+    attempts += 1;
+    if (guess > targetNum) {
         guess = parseInt(prompt("Guess Too High! Enter a new guess"));
     } else {
         guess = parseInt(prompt("Guess Too Low! Enter a new guess"));
     }
 }
 
-console.log("you win!")
+console.log(`You win! It took you ${attempts} guesses!`);
+
+//5. Optional: add number of guessing Attempts using attempts and increment the number of attempts after every failed guess.
