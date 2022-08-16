cb81f86c80c9f39863b502d194d48275ed373dec
ShanchaiZ <shanchai.zahid@gmail.com>
2022-08-16 18:29:21 -0400

    added user input verification using 'while' loop

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 //========================================================
 //1. Prompt user to enter a maximum number
 //2. Convert user input into an interger --> Console.log maximum to check function = Works!
-let maximum = parseInt(prompt("Enter the maximum number!"));
+let maximum = parseInt(prompt("Please Enter the maximum number!"));
 
-//Generate Random Number using the prompted number:
+//Verify if userInput number is a valid number using a WHILE loop. IF LOOP only checks validity ONCE. WHILE loop will check the validity of number continously.
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a VALID number please!"));
+};
+
+//3. Generate Random Number using the prompted number:
 let targetNum = Math.floor((Math.random() * maximum) + 1);
 console.log(targetNum);
